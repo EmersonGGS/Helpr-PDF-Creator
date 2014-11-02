01473aec36469f6a50d10fd86f2dfc04edcc86a2
EmersonGGS <n00618011@l209-n-014.mediastudies.ca>
2014-11-02 12:34:42 -0500

    Initial Commit

---
+++ PDFCreator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PDFCreator
+//
+//  Created by Stewart Emerson on 11/2/14.
+//  Copyright (c) 2014 Emerson Stewart. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PDFCreatorTests/PDFCreatorTests.m
@@ -0,0 +1,34 @@
+//
+//  PDFCreatorTests.m
+//  PDFCreatorTests
+//
+//  Created by Stewart Emerson on 11/2/14.
+//  Copyright (c) 2014 Emerson Stewart. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PDFCreatorTests : XCTestCase
+
+@end
+
+@implementation PDFCreatorTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
